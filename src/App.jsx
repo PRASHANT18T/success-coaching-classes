24857488a4477267d123a6a1fa5403ad3145f55e
PRASHANT18T <mrprashant18t@gmail.com>
2025-12-31 23:52:39 +0530

    solved path error in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Analytics } from "@vercel/analytics/react"
-import ScrollToTop from "./Components/ScrollToTop"
+import ScrollToTop from "./components/ScrollToTop"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
