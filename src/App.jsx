1287c1fa85cac0cd82376a2539f71a5ad34f2f0a
PRASHANT18T <mrprashant18t@gmail.com>
2025-12-31 23:44:32 +0530

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
+import ScrollToTop from "./Components/portfolioComponents/ScrollToTop"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -12,9 +13,11 @@ import Blog from './pages/Blog'
 
 function App() {
   return (
+    <>   
+      <ScrollToTop />
+        <Analytics />
+     <Router>
     
-    <Router>
-      <Analytics />
       <div className="min-h-screen flex flex-col">
         <Navbar />
         <AnimatePresence mode="wait">
@@ -30,6 +33,8 @@ function App() {
         <Footer />
       </div>
     </Router>
+    </>
+
   )
 }
 
