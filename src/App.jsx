b42d34eb4d61f3ed3bdc4339c79e9e408bdb8766
PRASHANT18T <mrprashant18t@gmail.com>
2025-12-31 23:49:04 +0530

    add new scrolltotop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import { Analytics } from "@vercel/analytics/react"
-import ScrollToTop from "./Components/portfolioComponents/ScrollToTop"
+import ScrollToTop from "./Components/ScrollToTop"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
